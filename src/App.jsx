fad20a01aabe78e533098e8a76acb99b7cada50f
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-07-14 15:46:21 +0700

    Update Prevent Flicker, Dark and Light mode with themes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,28 @@ import { AccessAlarm, ThreeDRotation, Home } from '@mui/icons-material'
 import Button from '@mui/material/Button'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
 function App() {
   // console.log(count);
 
+  function ModeToggle() {
+    const { mode, setMode } = useColorScheme()
+    return (
+      <Button
+        onClick={() => {
+          setMode(mode === 'light' ? 'dark' : 'light')
+        }}
+      >
+        {mode === 'light' ? 'Turn dark' : 'Turn light'}
+      </Button>
+    )
+  }
+
   return (
     <>
+      <ModeToggle />
+      <hr />
       <span>helloworld</span>
 
       <Typography variant="body2" color="text.secondary">Test Typography</Typography>
